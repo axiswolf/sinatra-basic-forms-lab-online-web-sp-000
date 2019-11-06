198f0c901dae588b763b8952e4d5cb00b56ff30f
Karen V <karenvo@usc.edu>
2019-11-06 08:37:24 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,8 +1,12 @@
 class Puppy
   attr_accessor :name, :breed, :months_old
+  @@puppies = []
   def initialize(name, breed, months_old)
     @name = name
     @breed = breed
     @months_old = months_old
+  end
+  def save
+
   end
 end
