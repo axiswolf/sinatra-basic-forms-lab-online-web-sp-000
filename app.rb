39f6e56f262c50a8a6cba0c8f98f25d61237d0f6
Karen V <karenvo@usc.edu>
2019-11-06 08:24:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,8 +11,7 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    @info = Puppy.new(params[:name], params[:breed], params[:age])
+    @info = Puppy.new(params[:name], params[:breed], params[:months_old])
     erb :display_puppy
   end
-
 end
